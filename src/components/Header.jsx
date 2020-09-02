39ed375c9d1f7f29bf3d888531ee3316ee93e973
nwease <nickwease@gmail.com>
2020-09-02 19:26:32 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../css/Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header__left">
+
+            </div>
+
+            <div className='header__center'>
+
+            </div>
+
+            <div className='header__right'>
+
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD50rkyBWrQl_cOMm6dzRPC50GPrqI_XfQ",
+    authDomain: "face-nick.firebaseapp.com",
+    databaseURL: "https://face-nick.firebaseio.com",
+    projectId: "face-nick",
+    storageBucket: "face-nick.appspot.com",
+    messagingSenderId: "751239694360",
+    appId: "1:751239694360:web:50af4986bbacaf72abdb6d",
+    measurementId: "G-82SJPCBVB2"
+};
