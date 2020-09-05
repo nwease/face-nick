53f854c3b85936ab1b5230d8cc466633ea4e5113
nwease <nickwease@gmail.com>
2020-09-05 11:37:24 +0200

    Began setup of Message component.

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import '../css/Message.css';
+import Avatar from '@material-ui/core/Avatar';
 
 const Message = () => {
     return (
         <div className='message'>
-            MESSAGE
+            <div className='message__top'>
+                <Avatar />
+
+                <form action=''>
+                    <input className='messageSender__input' placeholder={`Whats on your mind`} />
+                    <input placeholder='image URL (optional' />
+
+                    <button onClick={handleSubmit} type='submit'>
+                        Hidden Submit
+                    </button>
+                </form>
+            </div>
+
+            <div className='message__bottom'>
+
+            </div>
         </div>
     );
 };
