24f540e7a46a4aa50125e7d5ad52b6e0da218079
nwease <nickwease@gmail.com>
2020-09-05 20:01:06 +0200

    Styling the Message component.

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import '../css/Message.css';
 import Avatar from '@material-ui/core/Avatar';
+import VideocamIcon from '@material-ui/icons/Videocam';
+import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 
 const Message = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <div className='message'>
             <div className='message__top'>
                 <Avatar />
 
                 <form action=''>
-                    <input className='messageSender__input' placeholder={`Whats on your mind`} />
+                    <input className='messageSender__input' placeholder={`Whats on your mind?`} />
                     <input placeholder='image URL (optional' />
 
                     <button onClick={handleSubmit} type='submit'>
@@ -19,7 +27,29 @@ const Message = () => {
             </div>
 
             <div className='message__bottom'>
+                <div className='messageSender__option'>
+                    <VideocamIcon style={{color: 'red'}} />
 
+                    <h3>
+                        Live Video
+                    </h3>
+                </div>
+
+                <div className='messageSender__option'>
+                    <PhotoLibraryIcon style={{color: 'green'}} />
+
+                    <h3>
+                        Photo/Video
+                    </h3>
+                </div>
+
+                <div className='messageSender__option'>
+                    <InsertEmoticonIcon style={{color: 'red'}} />
+
+                    <h3>
+                        Feeling/Activity
+                    </h3>
+                </div>
             </div>
         </div>
     );
